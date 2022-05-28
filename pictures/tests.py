ce6af239713b16758129ca12ddcc6d1816fb1796
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-28 18:41:15 +0300

    ft: add class to test category

--- pictures/tests.py
+++ pictures/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+class CategoryTestCase(TestCase):
+    '''method to create instance before each test is run '''
+    def setUp(self):
+        self.animals= Category(category = "animals")
+        
+    '''Testing  instance'''
+    def test_instance(self):
+        self.assertTrue(isinstance(self.animals,Category))
+    
+    def test_save_location(self):
+        self.animals.save_category()
+        category= Category.objects.all()
+        self.assertTrue(len(category)>0)
+        
