c2f8d5ee2460a6b0974ede59563d7cbb19af8ef6
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-28 18:42:38 +0300

    ft: add class to test location

--- pictures/tests.py
+++ pictures/tests.py
@@ -14,4 +14,19 @@ class CategoryTestCase(TestCase):
         self.animals.save_category()
         category= Category.objects.all()
         self.assertTrue(len(category)>0)
-        
+
+
+class LocationTest(TestCase):
+    '''method to create instance before each test is run '''
+
+    def setUp(self):
+        self.nairobi = Location(location='Nairobi')
+    '''Testing  instance'''
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Location))
+
+    def test_save_location(self):
+        self.nairobi.save_location()
+        location= Location.objects.all()
+        self.assertTrue(len(location))
+
