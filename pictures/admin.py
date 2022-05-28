7276c3f360dfef0f19a404ea179a553fef28e60a
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-28 18:44:50 +0300

    ft: add models to admin.py

--- pictures/admin.py
+++ pictures/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Galore,Location,Category
 # Register your models here.
+
+admin.site.register(Galore)
+admin.site.register(Location)
+admin.site.register(Category)
