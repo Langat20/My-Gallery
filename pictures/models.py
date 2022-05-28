f1bee07b5e24b8421e1686e608c0b7419b5d14c4
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-28 17:59:21 +0300

    ft: add model class location

--- pictures/models.py
+++ pictures/models.py
@@ -11,3 +11,13 @@ class Category(models.Model):
         self.save()
     def delete_category(self):
         self.delete()
+        
+
+class Location(models.Model):
+    location = models.CharField(max_length =30)
+    def __str__(self):
+        return self.location
+    def save_location(self):
+        self.save()
+    def delete_location(self):
+        self.delete()
