086f99602b95dc1337e8af72434aa646300ca86a
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-28 18:00:48 +0300

    ft: add model class galore

--- pictures/models.py
+++ pictures/models.py
@@ -21,3 +21,21 @@ class Location(models.Model):
         self.save()
     def delete_location(self):
         self.delete()
+
+
+class Galore(models.Model):
+    title = models.CharField(max_length=60)
+    image = CloudinaryField('image/', default="")
+    description = models.TextField()
+    location = models.ManyToManyField('location')
+    category = models.ManyToManyField('category')
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save_galleria(self):
+        self.save()
+    def delete_galleria(self):
+        self.delete()
+        
