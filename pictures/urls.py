3840ff9af71aa7b443ad1ab1097f3a2cd949863a
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-28 19:00:59 +0300

    ft: add urls patterns

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
+urlpatterns = [
+    path('',views.main,name = 'main'),
+    path('search/', views.search_results, name='search_results'),
+    path('location/', views.photo_location, name='photo_location'),
+    path('imagedetails/<int:galore_id>', views.one_image, name='imagedetails'),
 
-urlpatterns=[
-    path('',views.welcome,name='welcome'),
-    
 ]
+
+#serve uploaded images on the development server 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
