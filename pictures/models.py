dcb1700fbbd127c2207e8af26192906e63da2a22
Langat20 <kipkuruie7.lang@gmail.com>
2022-05-28 13:22:46 +0300

    create model class category

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
+import datetime as dt
 # Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(max_length =30)
+    def __str__(self):
+        return self.category
+    def save_category(self):
+        self.save()
+    def delete_category(self):
+        self.delete()
